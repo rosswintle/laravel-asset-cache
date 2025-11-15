33d896fa2d8da8a600a89404ac8de23cc59c53b4
Ross Wintle <rosswintle@users.noreply.github.com>
2025-11-15 20:46:07 +0000

    Update test infrastructure

--- tests/BladeDirectivesTest.php
+++ tests/BladeDirectivesTest.php
@@ -2,17 +2,19 @@
 
 namespace RossWintle\LaravelAssetCache\Tests;
 
+use PHPUnit\Framework\Attributes\Test;
+
 class BladeDirectivesTest extends BladeTestCase
 {
 
-	/** @test */
-	public function the_blade_directive_returns_a_url()
-	{
+    #[Test]
+    public function the_blade_directive_returns_a_url()
+    {
         $blade = "@jscript('jquery', '3.2.1', 'dist/jquery.min.js')";
 
         $expected = '"<script src=\"" . \RossWintle\LaravelAssetCache\Facades\LaravelAssetCache::cachedAssetUrl(\'jquery\', \'3.2.1\', \'dist/jquery.min.js\') . "\"></script>';
 
         $this->assertStringContainsString($expected, $this->blade->compileString($blade));
-	}
+    }
 
 }
--- tests/RemoteAssetUrlTest.php
+++ tests/RemoteAssetUrlTest.php
@@ -3,49 +3,50 @@
 namespace RossWintle\LaravelAssetCache\Tests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 use RossWintle\LaravelAssetCache\LaravelAssetCache;
 
 class RemoteAssetUrlTest extends TestCase
 {
 
-	/** @test */
-	public function the_helper_returns_a_url_when_given_all_parameters()
-	{
-		$assetCache = new LaravelAssetCache();
-		$url = $assetCache->remoteAssetUrl('jquery', '3.2.1', 'dist/jquery.min.js');
-		$this->assertSame('https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js', $url);
-	}
+    #[Test]
+    public function the_helper_returns_a_url_when_given_all_parameters(): void
+    {
+        $assetCache = new LaravelAssetCache();
+        $url = $assetCache->remoteAssetUrl('jquery', '3.2.1', 'dist/jquery.min.js');
+        $this->assertSame('https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js', $url);
+    }
 
-	/** @test */
-	public function the_helper_returns_a_url_when_given_no_filename()
-	{
-		$assetCache = new LaravelAssetCache();
-		$url = $assetCache->remoteAssetUrl('jquery', '3.2.1');
-		$this->assertSame('https://cdn.jsdelivr.net/npm/jquery@3.2.1', $url);
-	}
+    #[Test]
+    public function the_helper_returns_a_url_when_given_no_filename()
+    {
+        $assetCache = new LaravelAssetCache();
+        $url = $assetCache->remoteAssetUrl('jquery', '3.2.1');
+        $this->assertSame('https://cdn.jsdelivr.net/npm/jquery@3.2.1', $url);
+    }
 
-	/** @test */
-	public function the_helper_returns_a_url_when_given_no_version()
-	{
-		$assetCache = new LaravelAssetCache();
-		$url = $assetCache->remoteAssetUrl('jquery', '', 'dist/jquery.min.js');
-		$this->assertSame('https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js', $url);
-	}
+    #[Test]
+    public function the_helper_returns_a_url_when_given_no_version()
+    {
+        $assetCache = new LaravelAssetCache();
+        $url = $assetCache->remoteAssetUrl('jquery', '', 'dist/jquery.min.js');
+        $this->assertSame('https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js', $url);
+    }
 
-	/** @test */
-	public function the_helper_returns_a_url_when_given_no_filename_or_version()
-	{
-		$assetCache = new LaravelAssetCache();
-		$url = $assetCache->remoteAssetUrl('jquery');
-		$this->assertSame('https://cdn.jsdelivr.net/npm/jquery', $url);
-	}
+    #[Test]
+    public function the_helper_returns_a_url_when_given_no_filename_or_version()
+    {
+        $assetCache = new LaravelAssetCache();
+        $url = $assetCache->remoteAssetUrl('jquery');
+        $this->assertSame('https://cdn.jsdelivr.net/npm/jquery', $url);
+    }
 
-	/** @test */
-	public function the_helper_returns_a_correct_url_when_slash_included_in_filename()
-	{
-		$assetCache = new LaravelAssetCache();
-		$url = $assetCache->remoteAssetUrl('jquery', '3.2.1', '/dist/jquery.min.js');
-		$this->assertSame('https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js', $url);
-	}
+    #[Test]
+    public function the_helper_returns_a_correct_url_when_slash_included_in_filename()
+    {
+        $assetCache = new LaravelAssetCache();
+        $url = $assetCache->remoteAssetUrl('jquery', '3.2.1', '/dist/jquery.min.js');
+        $this->assertSame('https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js', $url);
+    }
 
 }
